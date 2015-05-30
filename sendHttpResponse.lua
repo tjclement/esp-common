b7829eac06aa64495908423c1bf29a49896063fc
Tom Clement <mr@tomclement.nl>
2015-05-30 16:36:33 +0200

    Removed print statement in sendHttpReponse util

--- sendHttpResponse.lua
+++ sendHttpResponse.lua
@@ -1,5 +1,4 @@
 local function sendHttpResponse(connection, data)
-    print("sending data of length " .. data:len())
     connection:send("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/html; charset=ISO-8859-4\r\nContent-Length: " .. data:len() .. "\r\n\r\n" .. data)
 end
 
