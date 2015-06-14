9bd95770e430f80f5562878d10b7ce25cd5fa728
Tom Clement <mr@tomclement.nl>
2015-06-14 23:07:17 +0200

    Set CORS header in http response to allow calling the modules' API from web applications other than its built-in demo html

--- sendHttpResponse.lua
+++ sendHttpResponse.lua
@@ -1,5 +1,5 @@
 local function sendHttpResponse(connection, data)
-    connection:send("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/html; charset=ISO-8859-4\r\nContent-Length: " .. data:len() .. "\r\n\r\n" .. data)
+    connection:send("HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nConnection: close\r\nContent-Type: text/html; charset=ISO-8859-4\r\nContent-Length: " .. data:len() .. "\r\n\r\n" .. data)
 end
 
 return sendHttpResponse
